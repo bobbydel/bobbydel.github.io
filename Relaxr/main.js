3ff3f2fb310262a27866ad8b4a9c0752cd45ef1d
Bobby <bobbydeltondo90@gmail.com>
2018-07-08 15:37:25 -0700

    Added Projects section

---
+++ Relaxr/main.js
@@ -0,0 +1,33 @@
+$(document).ready(documentready);
+
+function documentready() {
+
+    $(".learnmore").click(LearnMoreClick);
+}
+//learn more
+function LearnMoreClick() {
+    event.preventDefault();
+    $("#learnmoretext").slideDown();
+    $(".learnmore").hide();
+}
+//read more
+$(".readmore").click(ReadMoreClick);
+
+function ReadMoreClick() {
+    event.preventDefault();
+    $("#show-this-on-click").slideDown();
+    $(".readmore").hide();
+    $(".readless").show();
+}
+//read less
+$(".readless").click(ReadLessClick);
+
+function ReadLessClick() {
+    event.preventDefault();
+    $(".readless").hide();
+    $("#show-this-on-click").slideUp();
+    $(".readmore").show();
+
+}
+
+$(".class").click(ReadMoreClick);
